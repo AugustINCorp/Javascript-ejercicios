6c9d9b667585fdba0e71e90ff921635252814cdd
Veroncho <augustoveron41@gmail.com>
2024-07-08 18:23:33 -0300

    Rama main

---
+++ js/ejercicioN4.js
@@ -0,0 +1,2 @@
+const nombreUsuario = prompt('Introduce tu nombre de Usuario:')
+console.log('Hola ' + nombreUsuario);
---
+++ js/ejercicioN5.js
@@ -0,0 +1,5 @@
+let numero = 10
+numero = 10 + 5
+document.write('Suma: ' + numero)
+
+// Preguntar por slack si esta bien hecho el ejercicio
