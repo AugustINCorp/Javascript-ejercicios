885655bae8435275b1777f4416b3092fb46283f1
Veroncho <augustoveron41@gmail.com>
2024-07-08 18:33:16 -0300

    se agregaron loe ejercicios 8 y 9

---
+++ js/ejercicioN9.js
@@ -0,0 +1,22 @@
+/*Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+
+Ejemplo:
+
+input: Hola mundo
+Output: oauo
+ */
+let frase = prompt("Ingrese una frase:");
+let vocales = "";
+
+for (let i = 0; i < frase.length; i++) {
+    let caracter = frase.charAt(i).toLowerCase();
+
+    if (caracter === 'a' || caracter === 'e' || caracter === 'i' || caracter === 'o' || caracter === 'u') {
+        if (!vocales.includes(caracter)) {
+            vocales += caracter + " ";
+        }
+    }
+}
+
+console.log("Las vocales que aparecen en la frase son: " + vocales.trim());
